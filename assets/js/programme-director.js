50555cb3a6c393494ac153ffde0f9abadcd587e0
Rahul Dawpersad <rahuldawpersad@gmail.com>
2025-02-19 16:07:48 +0200

    Added dropdown menu for services with responsive design

---
+++ assets/js/programme-director.js
@@ -0,0 +1,31 @@
+// const menuOpenButton = document.querySelector("#menu-open-button");
+// const menuCloseButton = document.querySelector("#menu-close-button");
+// const navLinks = document.querySelectorAll(".nav-menu .nav-link");
+
+
+// menuOpenButton.addEventListener("click", () => {
+//     // Toggle Mobile Menu Visibility
+//     document.body.classList.toggle("show-mobile-menu");
+// });
+
+// // Close menu when the close button is clicked
+// menuCloseButton.addEventListener("click", () => menuOpenButton.click());
+
+// // Close menu when the nav link is clicked
+// navLinks.forEach(link => {
+//     link.addEventListener("click", () => menuOpenButton.click());
+// });
+
+
+
+// let slides = document.querySelectorAll(".slide");
+// let index = 0;
+
+// function nextSlide() {
+//     slides[index].classList.remove("active");
+//     index = (index + 1) % slides.length;
+//     slides[index].classList.add("active");
+// }
+
+// setInterval(nextSlide, 5000); 
+
